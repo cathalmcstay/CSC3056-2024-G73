82fbb6135fbffa71a13b058d07c61eb23c11607a
Cathal McStay <cmcstay06@qub.ac.uk>
2024-02-07 13:19:11 +0000

    Staging for User, Transaction and Account Plus TODO 1 & 2

---
+++ src/tests/UserTests.java
@@ -0,0 +1,60 @@
+package tests;
+
+import model.User;
+import utils.TestUtils;
+
+public class UserTests {
+
+	public static void main(String[] args) {
+		testUserConstructor();
+
+	}
+	
+	public static void testUserConstructor() {
+        String test_username = "Mike";
+        String test_password = "my_password";
+        String test_first_name = "Mike";
+        String test_last_name = "Smith";
+        String test_mobile_number = "07428030031";
+
+        User testUser = new User(test_username, test_password, test_first_name, test_last_name, test_mobile_number);
+
+        System.out.println("Starting the assertions of the test method: testUserConstructor");
+
+        // Testing username
+        if (testUser.getUsername() == (test_username)) {
+            System.out.println(TestUtils.TEXT_COLOR_GREEN + "TC1-getUsername-Passed" + TestUtils.TEXT_COLOR_RESET);
+        } else {
+            System.out.println(TestUtils.TEXT_COLOR_RED + "TC1-getUsername-FAILED" + TestUtils.TEXT_COLOR_RESET);
+        }
+
+        // Testing password
+        if (testUser.getPassword() == (test_password)) {
+            System.out.println(TestUtils.TEXT_COLOR_GREEN + "TC2-getPassword-Passed" + TestUtils.TEXT_COLOR_RESET);
+        } else {
+            System.out.println(TestUtils.TEXT_COLOR_RED + "TC2-getPassword-FAILED" + TestUtils.TEXT_COLOR_RESET);
+        }
+
+        // Testing first name
+        if (testUser.getFirst_name() == (test_first_name)) {
+            System.out.println(TestUtils.TEXT_COLOR_GREEN + "TC3-getFirst_name-Passed" + TestUtils.TEXT_COLOR_RESET);
+        } else {
+            System.out.println(TestUtils.TEXT_COLOR_RED + "TC3-getFirst_name-FAILED" + TestUtils.TEXT_COLOR_RESET);
+        }
+
+        // Testing last name
+        if (testUser.getLast_name() == (test_last_name)) {
+            System.out.println(TestUtils.TEXT_COLOR_GREEN + "TC4-getLast_name-Passed" + TestUtils.TEXT_COLOR_RESET);
+        } else {
+            System.out.println(TestUtils.TEXT_COLOR_RED + "TC4-getLast_name-FAILED" + TestUtils.TEXT_COLOR_RESET);
+        }
+
+        // Testing mobile number
+        if (testUser.getMobile_number() == (test_mobile_number)) {
+            System.out.println(TestUtils.TEXT_COLOR_GREEN + "TC5-getMobile_number-Passed" + TestUtils.TEXT_COLOR_RESET);
+        } else {
+            System.out.println(TestUtils.TEXT_COLOR_RED + "TC5-getMobile_number-FAILED" + TestUtils.TEXT_COLOR_RESET);
+        }
+    }
+
+}
