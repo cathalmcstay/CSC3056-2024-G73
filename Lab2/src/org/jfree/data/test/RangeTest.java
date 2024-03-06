a7a456eae843196ba4cd75d238d2e2d86f69daf4
Justin Yuann <justinyuann@Justins-MacBook-Pro.local>
2024-03-06 19:23:13 +0000

    Completed tests for getLowerBound,getUpperBound and toString

--- Lab2/src/org/jfree/data/test/RangeTest.java
+++ Lab2/src/org/jfree/data/test/RangeTest.java
@@ -216,4 +216,268 @@ public class RangeTest {
 		double constrainedValue = range.constrain(Double.NaN);
 	}
 
+	// Method Name: getLowerBound()
+
+	// TC27: Testing 2 positive whole numbers
+	@Test
+	public void testGetLowerBoundPositiveWholeNumbers() {
+		Range range = new Range(4, 9);
+		assertEquals("getLowerBound: Testing 2 positive whole numbers", 4.0, range.getLowerBound(), 0.01);
+	}
+
+	// TC28: Testing 2 positive numbers with maximum input
+	@Test
+	public void testGetLowerBoundPositiveNumbersWithMaxInput() {
+		Range range = new Range(4, 2147483647);
+		assertEquals("getLowerBound: Testing 2 positive numbers with maximum input", 4.0, range.getLowerBound(), 0.01);
+	}
+
+	// TC29: Testing 2 negative whole numbers
+	@Test
+	public void testGetLowerBoundNegativeWholeNumbers() {
+		Range range = new Range(-9, -6);
+		assertEquals("getLowerBound: Testing 2 negative whole numbers", -9.0, range.getLowerBound(), 0.01);
+	}
+
+	// TC30: Testing 2 whole negative numbers with negative maximum input
+	@Test
+	public void testGetLowerBoundNegativeNumbersWithNegativeMaxInput() {
+		Range range = new Range(-2147483647, 6);
+		assertEquals("getLowerBound: Testing 2 whole negative numbers with negative maximum input", -2147483647.0,
+				range.getLowerBound(), 0.01);
+	}
+
+	// TC31: Testing 2 positive floating numbers
+	@Test
+	public void testGetLowerBoundPositiveFloatingNumbers() {
+		Range range = new Range(4.76, 9.73);
+		assertEquals("getLowerBound: Testing 2 positive floating numbers", 4.76, range.getLowerBound(), 0.01);
+	}
+
+	// TC32: Testing 2 negative floating point
+	@Test
+	public void testGetLowerBoundNegativeFloatingPoint() {
+		Range range = new Range(-9.73, -5.76);
+		assertEquals("getLowerBound: Testing 2 negative floating point", -9.73, range.getLowerBound(), 0.01);
+	}
+
+	// TC33: Testing 1 positive and negative whole number
+	@Test
+	public void testGetLowerBoundPositiveAndNegativeWholeNumber() {
+		Range range = new Range(-9, 10);
+		assertEquals("getLowerBound: Testing 1 positive and negative whole number", -9.0, range.getLowerBound(), 0.01);
+	}
+
+	// TC34: Testing Zero and negative floating number
+	@Test
+	public void testGetLowerBoundZeroAndNegativeFloatingNumber() {
+		Range range = new Range(-8, 0);
+		assertEquals("getLowerBound: Testing Zero and negative floating number", -8.0, range.getLowerBound(), 0.01);
+	}
+
+	// TC35: Testing upper bound less than lower bound
+	@Test(expected = IllegalArgumentException.class)
+	public void testGetLowerBoundUpperBoundLessThanLowerBound() {
+		Range range = new Range(10, -23.45);
+		range.getLowerBound();
+	}
+
+	// TC36: Testing null as input with a valid input
+	@Test(expected = NullPointerException.class)
+	public void testGetLowerBoundNullAsInputWithValidInput() {
+		Range range = new Range((Double) null, 10);
+		range.getLowerBound();
+	}
+
+	// TC37: Testing 1 positive whole number with positive high floating number
+	@Test
+	public void testGetLowerBoundPositiveWholeNumberWithPositiveHighFloatingNumber() {
+		Range range = new Range(1, 9656.7378209372819);
+		assertEquals("getLowerBound: Testing 1 positive whole number with positive high floating number", 1.0,
+				range.getLowerBound(), 0.01);
+	}
+
+	// TC38: Testing when both values are Zero
+	@Test
+	public void testGetLowerBoundWhenBothValuesAreZero() {
+		Range range = new Range(0, 0);
+		assertEquals("getLowerBound: Testing when both values are Zero", 0.0, range.getLowerBound(), 0.01);
+	}
+
+	// Method Name: getUpperBound()
+
+	// TC38.5: Testing 2 positive whole numbers
+	@Test
+	public void testGetUpperBoundPositiveWholeNumbers() {
+		Range range = new Range(4, 9);
+		assertEquals("getUpperBound: Testing 2 positive whole numbers", 9.0, range.getUpperBound(), 0.01);
+	}
+
+	// TC39: Testing 2 positive numbers with maximum input
+	@Test
+	public void testGetUpperBoundPositiveNumbersWithMaxInput() {
+		Range range = new Range(4, 2147483647);
+		assertEquals("getUpperBound: Testing 2 positive numbers with maximum input", 2147483647.0,
+				range.getUpperBound(), 0.01);
+	}
+
+	// TC40: Testing 2 negative whole numbers
+	@Test
+	public void testGetUpperBoundNegativeWholeNumbers() {
+		Range range = new Range(-9, -6);
+		assertEquals("getUpperBound: Testing 2 negative whole numbers", -6.0, range.getUpperBound(), 0.01);
+	}
+
+	// TC41: Testing 2 whole negative numbers with negative maximum input
+	@Test
+	public void testGetUpperBoundNegativeNumbersWithNegativeMaxInput() {
+		Range range = new Range(-2147483647, 6);
+		assertEquals("getUpperBound: Testing 2 whole negative numbers with negative maximum input", 6.0,
+				range.getUpperBound(), 0.01);
+	}
+
+	// TC42: Testing 2 positive floating numbers
+	@Test
+	public void testGetUpperBoundPositiveFloatingNumbers() {
+		Range range = new Range(4.76, 9.73);
+		assertEquals("getUpperBound: Testing 2 positive floating numbers", 9.73, range.getUpperBound(), 0.01);
+	}
+
+	// TC43: Testing 2 negative floating point
+	@Test
+	public void testGetUpperBoundNegativeFloatingPoint() {
+		Range range = new Range(-9.73, -5.76);
+		assertEquals("getUpperBound: Testing 2 negative floating point", -5.76, range.getUpperBound(), 0.01);
+	}
+
+	// TC44: Testing 1 positive and negative whole number
+	@Test
+	public void testGetUpperBoundPositiveAndNegativeWholeNumber() {
+		Range range = new Range(-9, 10);
+		assertEquals("getUpperBound: Testing 1 positive and negative whole number", 10.0, range.getUpperBound(), 0.01);
+	}
+
+	// TC45: Testing Zero and negative floating number
+	@Test
+	public void testGetUpperBoundZeroAndNegativeFloatingNumber() {
+		Range range = new Range(-8, 0);
+		assertEquals("getUpperBound: Testing Zero and negative floating number", 0.0, range.getUpperBound(), 0.01);
+	}
+
+	// TC46: Testing upper bound less than lower bound
+	@Test(expected = IllegalArgumentException.class)
+	public void testGetUpperBoundUpperBoundLessThanLowerBound() {
+		Range range = new Range(10, -23.45);
+		range.getUpperBound();
+	}
+
+	// TC47: Testing null as input with a valid input
+	@Test(expected = NullPointerException.class)
+	public void testGetUpperBoundNullAsInputWithValidInput() {
+		Range range = new Range((Double) null, 10);
+		range.getUpperBound();
+	}
+
+	// TC48: Testing 1 positive whole number with positive high floating number
+	@Test
+	public void testGetUpperBoundPositiveWholeNumberWithPositiveHighFloatingNumber() {
+		Range range = new Range(1, 9656.7378209372819);
+		assertEquals("getUpperBound: Testing 1 positive whole number with positive high floating number",
+				9656.7378209372819, range.getUpperBound(), 0.01);
+	}
+
+	// TC49: Testing when both values are Zero
+	@Test
+	public void testGetUpperBoundWhenBothValuesAreZero() {
+		Range range = new Range(0, 0);
+		assertEquals("getUpperBound: Testing when both values are Zero", 0.0, range.getUpperBound(), 0.01);
+	}
+
+	// Method Name: toString()
+	// TC50: Testing 2 positive whole numbers
+	@Test
+	public void testToStringPositiveWholeNumbers() {
+		Range range = new Range(4, 9);
+		assertEquals("toString: Testing 2 positive whole numbers", "Range[4.0,9.0]", range.toString());
+	}
+
+	// TC51: Testing 2 positive numbers with maximum input
+	@Test
+	public void testToStringPositiveNumbersWithMaxInput() {
+		Range range = new Range(4, 2147483647);
+		assertEquals("toString: Testing 2 positive numbers with maximum input", "Range[4.0,2.147483647E9]",
+				range.toString());
+	}
+
+	// TC52: Testing 2 negative whole numbers
+	@Test
+	public void testToStringNegativeWholeNumbers() {
+		Range range = new Range(-9, -6);
+		assertEquals("toString: Testing 2 negative whole numbers", "Range[-9.0,-6.0]", range.toString());
+	}
+
+	// TC53: Testing 2 whole negative numbers with negative maximum input
+	@Test
+	public void testToStringNegativeNumbersWithNegativeMaxInput() {
+		Range range = new Range(-2147483647, 6);
+		assertEquals("toString: Testing 2 whole negative numbers with negative maximum input",
+				"Range[-2.147483647E9,6.0]", range.toString());
+	}
+
+	// TC54: Testing 2 positive floating numbers
+	@Test
+	public void testToStringPositiveFloatingNumbers() {
+		Range range = new Range(4.76, 9.73);
+		assertEquals("toString: Testing 2 positive floating numbers", "Range[4.76,9.73]", range.toString());
+	}
+
+	// TC55: Testing 2 negative floating point
+	@Test
+	public void testToStringNegativeFloatingPoint() {
+		Range range = new Range(-9.73, -5.76);
+		assertEquals("toString: Testing 2 negative floating point", "Range[-9.73,-5.76]", range.toString());
+	}
+
+	// TC56: Testing 1 positive and negative whole number
+	@Test
+	public void testToStringPositiveAndNegativeWholeNumber() {
+		Range range = new Range(-9, 10);
+		assertEquals("toString: Testing 1 positive and negative whole number", "Range[-9.0,10.0]", range.toString());
+	}
+
+	// TC57: Testing Zero and negative floating number
+	@Test
+	public void testToStringZeroAndNegativeFloatingNumber() {
+		Range range = new Range(-8, 0);
+		assertEquals("toString: Testing Zero and negative floating number", "Range[-8.0,0.0]", range.toString());
+	}
+
+	// TC58: Testing upper bound less than lower bound
+	@Test(expected = IllegalArgumentException.class)
+	public void testToStringUpperBoundLessThanLowerBound() {
+		Range range = new Range(10, -23.45);
+		range.toString();
+	}
+
+	// TC59: Testing null as input with a valid input
+	@Test(expected = NullPointerException.class)
+	public void testToStringNullAsInputWithValidInput() {
+		Range range = new Range((Double) null, 10);
+		range.toString();
+	}
+
+	// TC60: Testing 1 positive whole number with positive high floating number
+	@Test
+	public void testToStringPositiveWholeNumberWithPositiveHighFloatingNumber() {
+		Range range = new Range(1, 9656.7378209372819);
+		assertEquals("toString: Testing 1 positive whole number with positive high floating number",
+				"Range[1.0,9.656737820937282E3]", range.toString());
+	}
+
+	// TC61: Testing when both values are Zero
+	@Test
+	public void testToStringWhenBothValuesAreZero() {
+		Range range = new Range(0, 0);
+		assertEquals("toString: Testing when both values are Zero", "Range[0.0,0.0]", range.toString());
+	}
 }
