c4068da98ab389b4cffa74457f6dee4f75d742ab
Justin Yuann <justinyuann@Justins-MacBook-Pro.local>
2024-03-06 16:23:49 +0000

    getCentralValue intial unit tests added

--- Lab2/src/org/jfree/data/test/RangeTest.java
+++ Lab2/src/org/jfree/data/test/RangeTest.java
@@ -9,50 +9,58 @@ import org.junit.*;
 public class RangeTest {
 	
 	private Range rangeObjectUnderTest; 
-	
+		
 	@Before 
 	public void setUp() throws Exception { 
-		rangeObjectUnderTest = new Range(-1, 1); 
+		rangeObjectUnderTest = new Range(0, 0); 
 	} 
 	
 	@After
-	public void tearDown() throws Exception { 
-		
-	} 
-	
-	@Test
-	public void testCentralValueShouldBeZero() { 
-		assertEquals("The central value of -1 and 1 should be 0", 
-		0, rangeObjectUnderTest.getCentralValue(), 0.000000001d); 
-	}
-	
-	@Test
-	public void testGetLengthBothPositiveAndEqual() {
-	    Range r1 = new Range(2, 2);
-	    assertEquals("getLength: Did not return the expected outcome", 0.0, r1.getLength(), 0.000000001d);
+	public void tearDown(){
+		rangeObjectUnderTest = new Range(0, 0); 
 	}
 
-	@Test
-	public void testGetLengthBothPositiveAndNotEqual() {
-	    Range r2 = new Range(4, 9);
-	    assertEquals("getLength: Did not return the expected outcome", 5.0, r2.getLength(), 0.000000001d);
-	}
+	//Method Name: getCentralValue()
 	
-	@Test
-	public void testGetLengthBothNegativeAndEqual() {
-	    Range r3 = new Range(-99, -99);
-	    assertEquals("getLength: Did not return the expected outcome", 0.0, r3.getLength(), 0.000000001d);
-	}
-
-	@Test
-	public void testGetLengthBothNegativeAndNotEqual() {
-	    Range r4 = new Range(-11, -4);
-	    assertEquals("getLength: Did not return the expected outcome", 7.0, r4.getLength(), 0.000000001d);
-	}
-	
-	@Test
-	public void testGetLengthOnePositiveOneNegative() {
-	    Range r5 = new Range(-5, 3);
-	    assertEquals("getLength: Did not return the expected outcome", 8.0, r5.getLength(), 0.000000001d);
-	}
+    // TC1: Testing 2 positive whole numbers
+    @Test
+    public void testCentralValuePositiveWholeNumbers() {
+        rangeObjectUnderTest = new Range(4, 9);
+        assertEquals("getCentralValue: Testing 2 positive whole numbers", 6.5, rangeObjectUnderTest.getCentralValue(), 0.01);
+    }
+    
+    // TC2: Testing 2 positive numbers with maximum input
+    @Test
+    public void testCentralValuePositiveNumbersWithMaximumInput() {
+        rangeObjectUnderTest = new Range(4, 354748989);
+        assertEquals("getCentralValue: Testing 2 positive numbers with maximum input", 1.773744965E8, rangeObjectUnderTest.getCentralValue(), 0.01);
+    }
+    
+    // TC3: Testing 2 negative whole numbers
+    @Test
+    public void testCentralValueNegativeWholeNumbers() {
+        rangeObjectUnderTest = new Range(-9, -6);
+        assertEquals("getCentralValue: Testing 2 negative whole numbers", -7.5, rangeObjectUnderTest.getCentralValue(), 0.01);
+    }
+    
+    // TC4: Testing 2 whole negative numbers with negative maximum input
+    @Test
+    public void testCentralValueWholeNegativeNumbersWithNegativeMaximumInput() {
+        rangeObjectUnderTest = new Range(-354748989, 6);
+        assertEquals("getCentralValue: Testing 2 whole negative numbers with negative maximum input", -1.773744975E8, rangeObjectUnderTest.getCentralValue(), 0.01);
+    }
+    
+    // TC5: Testing 2 positive floating numbers
+    @Test
+    public void testCentralValuePositiveFloatingNumbers() {
+        rangeObjectUnderTest = new Range(4.76, 9.73);
+        assertEquals("getCentralValue: Testing 2 positive floating numbers", 7.245, rangeObjectUnderTest.getCentralValue(), 0.01);
+    }
+    
+    // TC6: Testing 2 negative floating point
+    @Test
+    public void testCentralValueNegativeFloatingNumbers() {
+        rangeObjectUnderTest = new Range(-9.73, -5.76);
+        assertEquals("getCentralValue: Testing 2 negative floating point", -7.745, rangeObjectUnderTest.getCentralValue(), 0.01);
+    }
 } 
